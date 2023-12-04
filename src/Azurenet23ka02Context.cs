399807544f48454458d0544cccd5a99f9a8088bc
Pauli Selenius <99865089+Mera212@users.noreply.github.com>
2023-12-04 13:52:54 +0200

    added EntityFrameworkCore

---
+++ src/Azurenet23ka02Context.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using src;
+
+namespace New.Namespace;
+
+public partial class Azurenet23ka02Context : DbContext
+{
+    public Azurenet23ka02Context()
+    {
+    }
+
+    public Azurenet23ka02Context(DbContextOptions<Azurenet23ka02Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Person> Persons { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=tcp:dbschoolproject.database.windows.net,1433;Initial Catalog=azurenet23ka02;Persist Security Info=False;User ID=pauli;Password=Moikkab2!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>(entity =>
+        {
+            entity.HasKey(e => e.Personid).HasName("PK__Persons__AA2CFFDD4F41F9E4");
+
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
