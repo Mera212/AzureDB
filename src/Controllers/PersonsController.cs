54a586a437682a1709cd7f4ee267b5b763327ffb
Pauli Selenius <99865089+Mera212@users.noreply.github.com>
2023-12-18 12:14:06 +0200

    add

--- src/Controllers/PersonsController.cs
+++ src/Controllers/PersonsController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using src;
+using src.Controllers;
 
 namespace src.Controllers
 {
